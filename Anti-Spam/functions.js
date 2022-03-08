d5e2586979d1b2c9bf1197dafefed5fb6a8966eb
SprityON <71904569+SprityON@users.noreply.github.com>
2022-03-08 11:09:52 +0100

    Updated
    Better and updated code

--- Anti-Spam/functions.js
+++ Anti-Spam/functions.js
@@ -1,45 +1,35 @@
+
 function spamCheck(msg, set, time) {
-    if (msg.member.roles.cache.find(role => role.name === 'Muted')) return
-
-    let bool = false // the bool will be set to true if a member was found in the set object
-
-    // 'times' is the amount of messages
-    let user = { id: msg.author.id, time: Date.now(), times: 2 }
-
+    // go through every user in the set Object
     for (let u of set) {
-        // sometimes the 'times' would say that it was not a number. this fixes it
-        if (isNaN(u.times)) u.times = 2
-
-        // if the u.id coincides with the msg.author.id, then run the following code
+        
+        // if a user was found, continue
         if (u.id === msg.author.id) {
-            bool = true // the bool was set to true because the member was found
-
-            if (u.times >= 5) {
+            if (u.times >= 4) {
                 msg.reply(`Woah, not so fast!`)
+                msg.member.timeout(10000)
 
-                let muterole = msg.guild.roles.cache.find(role => role.name === 'Muted')
-                if (!msg.member.roles.cache.has(muterole.id)) {
-                    msg.member.roles.add(muterole)
-
-                    // remove the role and the member from the set after 5 seconds
-                    setTimeout(() => {
-                        msg.member.roles.remove(muterole)
-                        set.delete(u)
-                        msg.channel.send('unmuted')
-                    }, 5000);
-                }
+                // reset set Object
+                u.time = Date.now()
+                u.times = 0
             } else if ((Date.now() - u.time) <= time) {
-                // if the amount of time is less then 'time', then...
                 u.times++
                 u.time = Date.now()
+            } else {
+                // if u.time is above 'time' variable, reset it
+                u.time = Date.now()
+                u.times = 1
             }
         }
     }
 
-    // if this bool remains false, then add the user to the set
-    if (bool === false) {
-        set.add(user)
-    }
+    let userInSet = false
+    set.forEach(u => { 
+        if (u.id === msg.author.id) userInSet = true 
+    })
+    
+    // 'times' is set to 1 instead of 0 because the member already has sent 1 message
+    if (!userInSet) set.add({ id: msg.author.id, time: Date.now(), times: 1 })
 }
 
 // export this file so we can require it in another file, in this case bot.js
